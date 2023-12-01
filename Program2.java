4ee1f0eb40304d5025fcf8da631e1f45916c8c3e
Sukshitha shetty <151371852+sukshithashet@users.noreply.github.com>
2023-12-01 19:17:18 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+class T1 extends Thread
+{
+    public void run()
+    {
+         for(;;)
+         {
+            int n;
+            try
+            {
+                Thread.sleep(1000);
+                n=(int)(Math.ceil)(Math.random()*10));
+                System.out.printl
+            }
+            }
+         }
+    }
+}
